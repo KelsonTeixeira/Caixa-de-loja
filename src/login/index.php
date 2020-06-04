18b6c53f724ad4c679f2211d921bcf47af71c8a8
KelsonTeixeira <txkelson@gmail.com>
2020-06-04 01:46:17 -0300

    login page css updated

--- src/login/index.php
+++ src/login/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="../css/general.css">
+    <link rel="shortcut icon" href="../img/CLAQUETE.png" type="image/x-icon">
     <title>RSFS - Login</title>
   </head>
   <body>
@@ -16,15 +17,19 @@
           <img src="../img/CLAQUETE.png" alt="claquete">
           <h1>RSFS</h1>
         </div>
+        
         <div class="link">
           <a href="../registro">Registrar-se</a>
         </div>
       </div>
+
       <form class="login-form" action="login.php" method="post">
         <label for="username">Username</label>
         <input type="text" name="username" id="username">
+
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
+
         <button type="submit">Login</button>
       </form>
     </div>
