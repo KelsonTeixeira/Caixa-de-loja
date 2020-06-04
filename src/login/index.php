013a2d35f2423504bf6b49d432ec35bea990ea81
KelsonTeixeira <txkelson@gmail.com>
2020-06-04 01:26:34 -0300

    login page created

---
+++ src/login/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../css/general.css">
+    <title>RSFS - Login</title>
+  </head>
+  <body>
+    <div class="login">
+      <div class="bar">
+        <div class="logo">
+          <img src="../img/CLAQUETE.png" alt="claquete">
+          <h1>RSFS</h1>
+        </div>
+        <div class="link">
+          <a href="../registro">Registrar-se</a>
+        </div>
+      </div>
+      <form class="login-form" action="login.php" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    
+  </body>
+</html>
